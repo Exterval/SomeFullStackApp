594ed918efd6e8cce665f1e413d83c0c5041ad71
Exterval <treblmao@gmail.com>
2026-02-14 00:15:11 +0800

    styling 1

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -14,14 +14,16 @@ const NavBar = () => {
     <header className='bg-gradient-to-r from-slate-300 to-slate-700'>
         <div className='mx-auto max-w-6xl p-5'>
             <div className="flex items-center justify-between">
-                {/** MAKE DROPDOWN */}
+                {/** MAKE DROPDOWN & ADD CRUD FUNCTION */}
                 <button className='hover:bg-slate-400 transition duration-300 ease-in-out rounded-sm' onClick={(e) => handleShowMenu(e)}><Menu /></button>
                 {// fix style
-                display && <div className='absolute left-52 translate-y-full rounded bg-slate-300 shadow-lg p-5 w-max'> 
+                display && <div className='absolute left-52 translate-y-24 rounded bg-slate-300 shadow-lg p-3 w-max'> 
                   <ul>
-                    <li>Test</li>
-                    <li>Test</li>
-                    <li>Test</li>
+                    <h3 className='font-semibold text-lg'>Menu</h3>
+                    <hr />
+                    <li>Add</li>
+                    <li>Update</li>
+                    <li>Delete</li>
                   </ul>
                   </div>}
                 <h1 className='font-normal text-2xl text-white'>App</h1>
